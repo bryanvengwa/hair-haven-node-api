fa98c410b6d35490bbb3661c5c4bfa5285c58a49
bryanvengwa <bmvengwa@gmail.com>
2024-04-02 00:28:40 +0200

    corrected logic from the get products function

--- controllers/products.mjs
+++ controllers/products.mjs
@@ -1,13 +1,12 @@
-import models from '../models/index.js'; 
+import models from '../models/index.js';
 
 export async function getProducts(request, response) {
-    console.log('function ran')
-    // response.send('products');
-    const products = await models.Product.findAll();
-    console.log(products)
-    if (products.length > 0){
-      return  response.send('no products')
-
-    }
-    response.send('jgjg')
+  console.log('function ran');
+  // response.send('products');
+  const products = await models.Product.findAll();
+  console.log(products);
+  if (products.length < 0) {
+    return response.send('no products');
+  }
+  response.send(products)
 }
