06d9b0952a2cc9e4e6e750e84bc20d92bfe5513c
bryanvengwa <bmvengwa@gmail.com>
2024-04-01 23:53:29 +0200

    added a store route

--- app.mjs
+++ app.mjs
@@ -3,5 +3,11 @@ import bodyParser from 'body-parser';
 const app = express();
 app.use(bodyParser.json());
 
+app.get('/', function(req,res){
+    console.log('hello world');
+    res.send('hello world')
+})
+// app.use('/api')
+// app.use('/store')
 export default app;
  
---
+++ routes/store.mjs
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/products',(request , response) => {
+    response.send("products");
+})
+
+
+
+export default router;
